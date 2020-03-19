bba53ee5c2a0618822fb4669e3ace63fdb1240bc
RahulRaj16 <raj673538@gmail.com>
2020-03-19 20:01:18 +0530

    front end implementation

---
+++ app/src/main/java/com/example/zerovirus/entryActivity.java
@@ -0,0 +1,19 @@
+package com.example.zerovirus;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class entryActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_entry);
+
+    }
+
+
+
+
+
+}
