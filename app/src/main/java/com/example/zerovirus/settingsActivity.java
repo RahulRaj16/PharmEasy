fc47516195326d933b3c5eb451d0e886e378bd3b
Thavachelvam Ajanthan <62323802+ajanthan6@users.noreply.github.com>
2020-04-29 20:29:57 +0530

    Update settingsActivity.java

--- app/src/main/java/com/example/zerovirus/settingsActivity.java
+++ app/src/main/java/com/example/zerovirus/settingsActivity.java
@@ -8,7 +8,7 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
-public class settingsActivity6 extends AppCompatActivity {
+public class settingsActivity extends AppCompatActivity {
 
     CardView editInfoGrid;
     CardView UpdatePwGrid;
