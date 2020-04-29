f39af5f24b067166a0bc7bd0192ab80ab5c0e0ae
Thavachelvam Ajanthan <62323802+ajanthan6@users.noreply.github.com>
2020-04-29 17:47:51 +0530

    Update DBHelper.java

--- DBHelper.java
+++ DBHelper.java
@@ -17,7 +17,7 @@ import java.util.List;
 
 public class DBHelper extends SQLiteOpenHelper {
 
-    public static final String DATABASE_NAME = "PharmEasyDB";
+    public static final String DATABASE_NAME = "ZeroVirusDB";
     private static final String TAG ="DBHelper" ;
 
     public DBHelper(Context context) {
