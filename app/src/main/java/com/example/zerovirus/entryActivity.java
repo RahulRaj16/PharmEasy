07ee4c64959f4733a05651c55858756c5a677747
Thavachelvam Ajanthan <62323802+ajanthan6@users.noreply.github.com>
2020-04-29 20:41:04 +0530

    Add files via upload

--- app/src/main/java/com/example/zerovirus/entryActivity.java
+++ app/src/main/java/com/example/zerovirus/entryActivity.java
@@ -1,14 +1,57 @@
-package com.example.zerovirus;
+package com.example.zerovirus.Activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.zerovirus.Database.DBHelper;
+import com.example.zerovirus.R;
 
 public class entryActivity extends AppCompatActivity {
+    DBHelper dbHelper;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_entry);
+
+        dbHelper = new DBHelper(this);
+
+//        Toast.makeText(getApplicationContext(),type,Toast.LENGTH_LONG).show();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+
+
+
+                String type  = dbHelper.gettype();
+                Intent launchIntent = new Intent();
+                if(type.equals("Pharmacy")){
+                    launchIntent.setClass(getApplicationContext(), MainActivity.class);
+                    startActivity(launchIntent);
+                }
+
+                else if(type.equals("Hospital")){
+                    launchIntent.setClass(getApplicationContext(), HospitalActivity.class);
+                    startActivity(launchIntent);
+                }
+                else if(type.equals("Patient")){
+                    launchIntent.setClass(getApplicationContext(), PatientsActivity.class);
+                    startActivity(launchIntent);
+                }
+                else if(type.equals("")){
+                    Intent loginIntent = new Intent(entryActivity.this,LoginActivity.class);
+                    startActivity(loginIntent);
+                }
+                finish();
+            }
+        }, 4750);
+
+
+
 
     }
 
