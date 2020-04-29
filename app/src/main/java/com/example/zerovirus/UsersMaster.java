885118da2a851612249dfa4d2b9d5f87adfa2488
Thavachelvam Ajanthan <62323802+ajanthan6@users.noreply.github.com>
2020-04-29 20:10:03 +0530

    Add files via upload

---
+++ app/src/main/java/com/example/zerovirus/UsersMaster.java
@@ -0,0 +1,58 @@
+package com.example.zerovirus.Database;
+
+import android.provider.BaseColumns;
+
+public class UsersMaster {
+
+    public UsersMaster() {
+    }
+
+    public  static class Users implements BaseColumns {
+        public static final String TABLE_NAME = "users";
+        public static final String COLUMN_NAME_USERNAME = "username";
+        public static final String COLUMN_NAME_PASSWORD = "password";
+        public static final String COLUMN_NAME_MOBILE = "mobile";
+        public static final String COLUMN_NAME_ADDRESS = "address";
+        public static final String COLUMN_NAME_TYPE = "type";
+        public static final String COLUMN_NAME_CURRENT = "current";
+    }
+
+    public static class Prescriptions implements BaseColumns {
+        public static final String TABLE_NAME = "prescriptions";
+        public static final String COLUMN_NAME_PATIENTNAME = "patientName";
+        public static final String COLUMN_NAME_DIAGNOSIS="diagnosis";
+        public static final String COLUMN_NAME_ADDRESS="address";
+        public static final String COLUMN_NAME_PHONE = "phone";
+        public static final String COLUMN_NAME_PRESCRIPTION="prescription";
+
+    }
+
+    public static class Orders implements BaseColumns {
+        public static final String TABLE_NAME = "orders";
+        public static final String COLUMN_NAME_CUSTOMER_NAME = "cusName";
+        public static final String COLUMN_NAME_PRESCRIPTION="prescription";
+        public static final String COLUMN_NAME_ADDRESS="address";
+        public static final String COLUMN_NAME_PHONE = "phone";
+
+    }
+
+    public static class Delivery implements BaseColumns {
+        public static final String TABLE_NAME = "delivery";
+        public static final String COLUMN_NAME_CUSTOMER_NAME = "cusName";
+        public static final String COLUMN_NAME_PRESCRIPTION="prescription";
+        public static final String COLUMN_NAME_ADDRESS="address";
+        public static final String COLUMN_NAME_PHONE = "phone";
+        public static final String COLUMN_NAME_STATUS = "status";
+        public static final String COLUMN_NAME_OWNER = "owner";
+
+    }
+
+
+    public static class Feedback implements BaseColumns {
+        public static final String TABLE_NAME = "feedback";
+        public static final String COLUMN_NAME_NAME = "name";
+        public static final String COLUMN_NAME_EMAIL="email";
+        public static final String COLUMN_NAME_MESSAGE="message";
+
+    }
+}
