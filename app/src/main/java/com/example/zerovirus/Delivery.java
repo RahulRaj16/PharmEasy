66c00e95a4e04dd9e3f99e649b5ac845a0f929f2
Thavachelvam Ajanthan <62323802+ajanthan6@users.noreply.github.com>
2020-04-29 20:14:21 +0530

    Add files via upload

--- app/src/main/java/com/example/zerovirus/Delivery.java
+++ app/src/main/java/com/example/zerovirus/Delivery.java
@@ -1,22 +1,34 @@
 package com.example.zerovirus.Model;
 
-public class Orders {
+public class Delivery {
     private Integer id;
-    private String customerName;
+    private String customer;
     private String prescription;
     private String address;
     private String phone;
+    private String status;
+    private String owner;
 
-    public Orders(Integer id, String customer, String prescription, String address, String phone ) {
+    public String getOwner() {
+        return owner;
+    }
+
+    public Delivery(Integer id, String customer, String prescription, String address, String phone, String status, String owner) {
         this.id = id;
-        this.customerName = customer;
+        this.customer = customer;
         this.prescription = prescription;
         this.address = address;
         this.phone = phone;
+        this.status = status;
+        this.owner = owner;
     }
 
     public Integer getId() {
         return id;
+    }
+
+    public String getStatus() {
+        return status;
     }
 
     public String getAddress() {
@@ -27,8 +39,8 @@ public class Orders {
         return phone;
     }
 
-    public String getCustomerName() {
-        return customerName;
+    public String getCustomer() {
+        return customer;
     }
 
     public String getPrescription() {
